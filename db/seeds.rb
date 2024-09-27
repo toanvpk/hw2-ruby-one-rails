6a8a261dbbf66a1f53a97fe616076dcb40a6588b
Toan Vu <toanvpk@yahoo.com>
2024-09-26 23:00:33 -0500

    adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,8 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
+    :release_date => '13-Aug-2021'},
+  {:title => 'CODA2', :rating => 'PG-13',
     :release_date => '13-Aug-2021'}
 ]
 
